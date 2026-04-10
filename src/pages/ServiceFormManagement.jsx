85982d1bf77bc7fdbd4a8616e7e67c0a8799e985
Shiksha1802 <shiksharawat1802@example.com>
2026-04-10 19:38:27 +0530

    final changes

--- src/pages/ServiceFormManagement.jsx
+++ src/pages/ServiceFormManagement.jsx
@@ -2095,6 +2095,23 @@ const CreateFormModal = ({ categories, forms, onClose, onSave }) => {
             </div>
           )}
 
+          {/* Show message when sub-services are enabled but none selected */}
+          {hasSubServices && !selectedSubService && subServices.length > 0 && (
+            <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4 mb-4">
+              <div className="flex items-center space-x-2">
+                <div className="text-yellow-600">
+                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <div>
+                  <h4 className="text-sm font-medium text-yellow-800">Select a Sub-Service</h4>
+                  <p className="text-sm text-yellow-700">Please select a sub-service above to add fields specific to that service type.</p>
+                </div>
+              </div>
+            </div>
+          )}
+
           {/* Add Fields */}
           {(!hasSubServices || selectedSubService) && (
             <div>
